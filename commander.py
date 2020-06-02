9d31c7ca36a487c03b2937761f7dcebd7e795acb
Kevin Haghi <kevin.haghi@gmail.com>
2020-06-01 20:43:19 -0700

    Add files via upload

--- commander.py
+++ commander.py
@@ -13,7 +13,10 @@ class Richard:
 class Character:
     def __init__(self,stats):
         #Initiate a character and their base stats
+        
         self.base_stats(stats)
+        self.attack_buffs([])
+        self.defense_buffs([])
     
     def base_stats(self,stats):
         #Base stats for character
@@ -31,10 +34,35 @@ class Character:
         self.sh = stats[11]
         
     def civilization(self,civ):
+        if civ == 'Rome':
+            self.defense_buffs([5,0,0])
         self.civ = civ
+        
+    def attack_buffs(self,buffs):
+        if buffs == []:
+            self.buffs_ia = 0
+            self.buffs_ca = 0
+            self.buffs_aa = 0
+        else:
+            self.buffs_ia += buffs[0]
+            self.buffs_ca += buffs[1]
+            self.buffs_aa += buffs[2]
+    
+    def defense_buffs(self,buffs):
+        if buffs == []:
+            self.buffs_id = 0
+            self.buffs_cd = 0
+            self.buffs_ad = 0
+        else:
+            self.buffs_id += buffs[0]
+            self.buffs_cd += buffs[1]
+            self.buffs_ad += buffs[2]
+    
+    
 
 stats = [0,1,2,3,4,5,6,7,8,9,10,11]
 
 kevin = Character(stats)
-print(kevin.aa,kevin.sh)
-        
+print(kevin.aa,kevin.sh,kevin.buffs_ia,kevin.buffs_id)
+kevin.civilization('Rome')
+print(kevin.aa,kevin.sh,kevin.buffs_ia,kevin.buffs_id)
